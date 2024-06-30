354575ad8093cbdfd6c04c9b473a56597632c849
Shyshkanova2005 <ivanovasasha883@gmail.com>
2024-06-30 16:58:07 +0300

    feat CW
    added CW

---
+++ multithreding/multithreding.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono> 
+using namespace std;
+
+void printMessage() {
+    cout << "Hello" << endl; 
+}
+
+int sumanum() {
+    int suma = 0;
+    for (int i = 0; i <= 10; ++i) {
+        suma += i;
+    cout << "Result: " << suma<< endl;
+    }
+    return suma;
+}
+int add(int a, int b) {
+    cout << "Result: " << a + b << endl;
+    return a + b;
+   
+}
+
+
+
+int main() {
+    thread t1(printMessage);  
+      
+    
+    thread t2(sumanum); 
+ 
+  
+    thread t3(add, 10, 10); 
+
+
+
+    t1.join(); 
+    t2.join();
+    t3.join();
+  
+
+    return 0;
+}
