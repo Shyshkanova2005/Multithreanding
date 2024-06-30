cbe990a563d09e1501436b2664504c6adeaeed01
Shyshkanova2005 <ivanovasasha883@gmail.com>
2024-06-30 17:12:37 +0300

    feat: add new project

---
+++ time/time.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono> 
+using namespace std;
+
+void longRunningTask(int n, int number) {
+    for (int i = 0; i < 5; ++i) {
+        cout << "Thread# " << number << std::endl;
+        cout << "Thread running " << i << std::endl;
+        this_thread::sleep_for(chrono::seconds(n));
+      
+    }
+}
+
+int main()
+{
+    thread t1(longRunningTask, 1, 1);
+    thread t2(longRunningTask, 4, 2);
+    thread t3(longRunningTask, 6, 3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
+
+
